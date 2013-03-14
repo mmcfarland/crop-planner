9f3245fceeeeb3707b42b4bb0b5aa9df3fb26dc0
Matthew McFarland <mmcfarland@gmail.com>
2013-03-13 20:38:21 -0400

    Open up the admin to the few models we start with

---
+++ crop_planner/planner/admin.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from planner.models import GardenSite, Crop, SiteUser, Variety, PlantingGuide
+
+admin.site.register(GardenSite)
+admin.site.register(Crop)
+admin.site.register(SiteUser)
+admin.site.register(Variety)
+admin.site.register(PlantingGuide)
