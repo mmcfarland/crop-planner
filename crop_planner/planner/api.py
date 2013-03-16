c47071f1be7324ef5a10b83b3065494b0bc62812
Matthew McFarland <mmcfarland@gmail.com>
2013-03-16 19:02:53 -0400

    Add session based authorization for GardenSite resources

--- crop_planner/planner/api.py
+++ crop_planner/planner/api.py
@@ -1,14 +1,54 @@
 from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
 from tastypie.authentication import SessionAuthentication
+from tastypie.exceptions import Unauthorized
 
 from planner.models import Crop, GardenSite, SiteUser, Variety, PlantingGuide
 from django.contrib.auth.models import User
+
+class ScopedGardenAuth(Authorization):
+
+    def read_list(self, object_list, bundle):
+        return object_list.filter(site=bundle.request.session['garden_site'])
+
+    def read_detail(self, object_list, bundle):
+        # bundle obj is the resource being requested
+        if bundle.obj.site != bundle.request.session['garden_site']:
+            raise Unauthorized()
+        return True
+
+    def create_list(self, object_list, bundle):
+        return object_list
+
+    def create_detail(self, object_list, bundle):
+        return bundle.obj.user == bundle.request.user
+
+    def update_list(self, object_list, bundle):
+        allowed = []
+
+        # Since they may not all be saved, iterate over them.
+        for obj in object_list:
+            if obj.user == bundle.request.user:
+                allowed.append(obj)
+
+        return allowed
+
+    def update_detail(self, object_list, bundle):
+        return bundle.obj.user == bundle.request.user
+
+    def delete_list(self, object_list, bundle):
+        # Sorry user, no deletes for you!
+        raise Unauthorized("Sorry, no deletes.")
+
+    def delete_detail(self, object_list, bundle):
+        raise Unauthorized("Sorry, no deletes.")
+
 
 class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
         resource_name = 'user'
-        excludes = ['email', 'password', 'is_active', 'is_staff', 'is_superuser']
+        excludes = ['password', 'is_staff', 'is_superuser']
         authentication = SessionAuthentication()
         allowed_methods = ['get']
 
@@ -26,7 +66,13 @@ class VarietyResource(ModelResource):
         queryset = Variety.objects.all()
         resource_name = 'variety'
         authentication = SessionAuthentication()
+        authorization = ScopedGardenAuth()
 
-    def apply_authorization_limits(self, request, object_list):
-        return object_list.filter(site=request.session.garden_site)
+class PlantingGuideResource(ModelResource):
+    class Meta:
+        queryset = PlantingGuide.objects.all()
+        resource_name = 'guide'
+        authentication = SessionAuthentication()
+        authorization = ScopedGardenAuth()
+
 
