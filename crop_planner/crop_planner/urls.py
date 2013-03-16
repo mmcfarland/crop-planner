c31a1a88c500653ab8c6fa70f1f308631a395e84
Matthew McFarland <mmcfarland@gmail.com>
2013-03-15 22:29:18 -0400

    Open up additional api resources

--- crop_planner/crop_planner/urls.py
+++ crop_planner/crop_planner/urls.py
@@ -1,15 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from tastypie.api import Api
-from planner.api import CropResource
+from planner.api import CropResource, VarietyResource, UserResource
 
 api = Api(api_name='v0.1')
 api.register(CropResource())
+api.register(VarietyResource())
+api.register(UserResource())
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^/', include('planner.urls')),
-    url(r'^api/', include(api.urls))
+    url(r'^', include('planner.urls')),
+    url(r'^api/', include(api.urls)),
 )
