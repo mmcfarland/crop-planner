acd4c817a5c7e069d46f9b581610850b3a349732
Matthew McFarland <mmcfarland@gmail.com>
2013-03-16 20:10:44 -0400

    Variety views

--- crop_planner/planner/static/js/Planner/Variety.js
+++ crop_planner/planner/static/js/Planner/Variety.js
@@ -8,5 +8,35 @@
         urlRoot: '/api/v0.1/variety/'
     });
 
+    N.views.VarietyListView = Backbone.View.extend({
+        _subViews: [],
+        tagName: 'ul',
+
+        initialize: function() {
+            this.listenTo(this.collection, "add remove sort", this.render);
+        },
+
+        render: function() {
+            this.$el.empty();
+            _.invoke(this._subViews, 'remove');
+            this.collection.each(function(variety) {
+                var view = new N.views.VarietyListItemView({model: variety});
+                _subViews.push(view);
+                this.$el.append(view.render().$el);
+            });
+        }
+    });
+
+    N.views.VarietyListItemView = Backbone.View.extend({
+        initialize: function() {
+            this.listenTo(this.model, "change", this.render);
+        },
+
+        render: function() {
+            var html = N.page.tmpl["tmpl-variety-list"](this.model);
+            this.$el.html(html);
+            return this;
+        }
+    });
 
 }(Planner));
