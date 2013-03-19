bd4fd63238cbb4e988e833469685b6f24a8ca8ff
Matthew McFarland <mmcfarland@gmail.com>
2013-03-18 23:06:54 -0400

    Add interval models to admin

--- crop_planner/planner/admin.py
+++ crop_planner/planner/admin.py
@@ -1,9 +1,13 @@
 
 from django.contrib import admin
-from planner.models import GardenSite, Crop, SiteUser, Variety, PlantingGuide
+from planner.models import GardenSite, Crop, SiteUser, Variety, PlantingGuide,\
+    IntervalHarvestPlan, IntervalHarvest, IntervalVariety
 
 admin.site.register(GardenSite)
 admin.site.register(Crop)
 admin.site.register(SiteUser)
 admin.site.register(Variety)
 admin.site.register(PlantingGuide)
+admin.site.register(IntervalHarvestPlan)
+admin.site.register(IntervalHarvest)
+admin.site.register(IntervalVariety)
